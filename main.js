dad22da2b16c2963430c34d56758f3e4c886340f
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2026-02-07 12:06:28 +0100

    snack-2

--- main.js
+++ main.js
@@ -50,3 +50,32 @@ function getPostId(id) {
 getPostId(5)
     .then(result => console.log(result))
     .catch(err => console.error(err))
+
+/*🏆 Snack 2
+Crea la funzione lanciaDado() che restituisce una Promise che, dopo 3 secondi, 
+genera un numero casuale tra 1 e 6. Tuttavia, nel 20% dei casi, il dado si "incastra" e 
+la Promise va in reject.*/
+
+function lanciaDado() {
+    return new Promise((resolve, reject) => {
+        console.log('sto lanciando il dado...')
+        setTimeout(() => {
+            const Incastrato = Math.random() < 0.2
+            if (Incastrato) {
+                reject('il dado si è incastrato, riprova.')
+            } else {
+                const risultato = Math.floor(Math.random() * 6) + 1
+                resolve(risultato)
+            }
+        }, 3000)
+    })
+}
+
+lanciaDado()
+    .then(result => console.log('il dado ha lanciato:', result))
+    .catch(err => console.error(err));
+
+
+/*🎯 Bonus: HOF con closure per memorizzare l'ultimo lancio
+Modifica la funzione in creaLanciaDado(), che restituisce una closure che memorizza l'ultimo risultato. 
+Se il numero esce due volte di fila, stampa "Incredibile!"*/
